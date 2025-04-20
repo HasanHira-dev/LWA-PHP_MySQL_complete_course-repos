a1b6bc985e78cc512478e02d3494f26a010238e2
HasanHira-dev <hasanhira.me@gmail.com>
2025-04-20 22:31:58 +0600

    Do some practice in string function file

--- 02. Class_02/2. basics_string_functions.php
+++ 02. Class_02/2. basics_string_functions.php
@@ -3,7 +3,33 @@
     // String Functions
     // - strlen()  = For string length
     $country = "Bangladesh is my country";
-    echo strlen($country);
+    // echo strlen($country);
+
+    // - strtoupper() = For string uppercase
+    $strup = strtoupper($country);
+    // echo $strup;
+
+    // - strtolower() = For string lowercase
+    $strlow = strtolower($country);
+    // echo $strlow;
+
+    // - ucwords() = For Capitalize string's words
+    $strupwords = ucwords($country);
+    // echo $strupwords;
+
+    // - trim() = For remove unusual spaces
+    $spacedstr = ' Ha! Ha!!    Ha!, I am a      Ghost, "You     can\'t  see    me   ."';
+    $trimstring = trim($spacedstr);
+    // echo $trimstring;
+
+    // - str_replace() = For replace string(s)
+    //   - str_replace(search, replace, string);
+    $strrep = str_replace("Ghost", "Magician", $trimstring);
+    echo $strrep;
+
+
+
+
 
 
 
